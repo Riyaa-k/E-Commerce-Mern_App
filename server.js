8c35baafdeb01acff486cab1943a352b15a32f83
Anshita <anshitakoshta28@gmail.com>
2023-08-22 16:02:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-// import path from 'path';
+import path from 'path';
 
 //configure env
 dotenv.config();
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 
 //routes
@@ -31,15 +31,15 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get('/',(req,res)=>{
-  res.send('hello world');
-})
+// app.get('/',(req,res)=>{
+//   res.send('hello world');
+// })
 
-// app.use('*',function(req,res){
-//   res.sendFile(path.join(__dirname,'./client/build/index.html'));
-// }
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+}
 
-// );
+);
 
 //PORT
 const PORT = process.env.PORT || 8080;
