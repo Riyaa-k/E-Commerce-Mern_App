fba7ddfeec8f0dd7958d6745e7cba95cde176725
Anshita <anshitakoshta28@gmail.com>
2023-08-22 15:58:56 +0530

    final changes

--- server.js
+++ server.js
@@ -7,18 +7,13 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
 
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
-
-//Es module fix
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -27,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './client/build')));
+// app.use(express.static(path.join(__dirname, './client/build')));
 
 
 //routes
@@ -36,15 +31,15 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-// app.get('/',(req,res)=>{
-//   res.send('hello world');
-// })
+app.get('/',(req,res)=>{
+  res.send('hello world');
+})
 
-app.use('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
-}
+// app.use('*',function(req,res){
+//   res.sendFile(path.join(__dirname,'./client/build/index.html'));
+// }
 
-);
+// );
 
 //PORT
 const PORT = process.env.PORT || 8080;
